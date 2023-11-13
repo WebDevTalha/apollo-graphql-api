95e8977a4705a6fe327eb1d147b61c0893703bf9
WebDevTalha <mdfarhantanvirtalha@gmail.com>
2023-11-13 18:53:31 +0600

    get single novel query

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -24,6 +24,7 @@ const typeDefs = `#graphql
      }
 
      type Query {
+          novel(id: ID!): Novel
           novels: [Novel]
      }
 `
@@ -31,6 +32,13 @@ const resolvers = {
     Query: {
         novels: async (parent: any, args: any, context: Context) => {
             return await context.prisma.novel.findMany()
+        },
+        novel: async (parent: any, args: any, context: Context) => {
+            return await context.prisma.novel.findUnique({
+                where: {
+                    id: args.id,
+                },
+            })
         },
     },
     Novel: {
